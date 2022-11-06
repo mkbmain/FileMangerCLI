46e847ed75d2433842b9f3db0ee2ed81afbfa5dd
mkbmain <michael.bourke@live.co.uk>
2022-11-06 05:34:39 +0000

    Multi display (#2)
    * MultiDisplay First Pass
    
    * part for last feature
    
    * All done

--- FileManagerCLI/FileManager/FileManagerWindow.cs
+++ FileManagerCLI/FileManager/FileManagerWindow.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Linq;
 using FileManagerCLI.Data;
 using FileManagerCLI.Enums;
 using FileManagerCLI.Utils;
@@ -7,17 +9,25 @@ namespace FileManagerCLI.FileManager
 {
     public class FileManagerWindow : FileManagerDisplay
     {
-        public FileManagerWindow(int maxWidth = int.MaxValue, int startLeft = 0) : base(maxWidth, startLeft)
+        public FileManagerWindow(decimal widthPercent = 1, decimal startLeftPercent = 0) : base(widthPercent,
+            startLeftPercent)
         {
+        }
+
+        public void UpdateDisplayDetails(decimal widthPercent, decimal startLeftPercent)
+        {
+            _widthPercent = widthPercent;
+            _startLeftPercentPercent = startLeftPercent;
+            Redraw();
         }
 
         public bool Copy()
         {
             if (Stored is null) return false;
-            
+
             var newPath = System.IO.Path.Combine(Path, Stored.Name);
             if (newPath == Stored.FullPath) return false;
-            
+
             switch (Stored.IoType)
             {
                 case IoItemType.File:
@@ -29,6 +39,7 @@ namespace FileManagerCLI.FileManager
                 default:
                     return false;
             }
+
             Path = Path;
             return true;
         }
@@ -108,6 +119,95 @@ namespace FileManagerCLI.FileManager
                     break;
             }
         }
+
+        public void EditLocation()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.Write(FitWidth(Path, false, Console.WindowWidth));
+            string path = Path;
+            var keys = "ABCDEFGHIJKLMNOPQRS\\/TUVWXYZ.-_".ToCharArray().GroupBy(e => e.ToString())
+                .ToDictionary(w => w.Key, w => w.First());
+            bool tab = false;
+            while (true)
+            {
+                Console.CursorVisible = true;
+                Console.BackgroundColor = Program.Config.ForegroundColor;
+                Console.ForegroundColor = Program.Config.BackgroundColor;
+                Console.SetCursorPosition(0, 0);
+                Console.Write(FitWidth(path, false, Console.WindowWidth));
+                
+                var key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.Enter:
+                        if (Directory.Exists(path))
+                        {
+                            Path = path;
+                            Console.CursorVisible = false;
+                            return;
+                        }
+
+                        break;
+                    case ConsoleKey.Tab:
+                        var partialPathParts = path.Split(FileIoUtil.PathSeparator);
+                        var partialPath = string.Join(FileIoUtil.PathSeparator,
+                            partialPathParts.Take(partialPathParts.Length - 1));
+                        if (!Directory.Exists(partialPath))
+                        {
+                            continue;
+                        }
+
+                        var items = FileIoUtil.GetDetailsForPath(partialPath)
+                            .Where(e => e.IoType == IoItemType.Directory).ToList();
+                        if (!items.Any())
+                        {
+                            continue;
+                        }
+
+                        if (tab)
+                        {
+                            var match = items.FirstOrDefault(w => w.Name == partialPathParts.Last());
+                            if (match is null)
+                            {
+                                tab = false;
+                                continue;
+                            }
+
+                            path = System.IO.Path.Combine(partialPath,
+                                match == items.Last() ? items.Last().Name : items[items.IndexOf(match) + 1].Name);
+                        }
+                        else
+                        {
+                            var item = items.FirstOrDefault(x => x.Name.StartsWith(partialPathParts.Last()));
+                            if (item is null)
+                            {
+                                continue;
+                            }
+
+                            tab = true;
+                            path = System.IO.Path.Combine(partialPath, item.Name);
+                        }
+                        break;
+
+                    case ConsoleKey.Delete:
+                    case ConsoleKey.Backspace:
+                        path = path[..^1];
+                        break;
+                    default:
+
+                        if (keys.ContainsKey(key.KeyChar.ToString().ToUpper()))
+                        {
+                            path += key.Modifiers.HasFlag(ConsoleModifiers.Shift)
+                                ? key.KeyChar.ToString().ToUpper()
+                                : key.KeyChar.ToString().ToLower();
+                        }
+
+                        break;
+                }
+            }
+        }
+
+        public void Redraw() => Display(DisplayItems, Offset);
 
         public void MoveSelected(bool up)
         {
--- FileManagerCLI/Program.cs
+++ FileManagerCLI/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.IO;
+using System.Linq;
 using FileManagerCLI.FileManager;
 using FileManagerCLI.Settings;
 
@@ -9,6 +13,17 @@ namespace FileManagerCLI
     {
         public static Config Config = new Config();
         private const string ConfigFileName = "config.json";
+        private Size Size = new Size(Console.WindowWidth, Console.WindowHeight);
+
+        private static void ChangeDisplays(IReadOnlyList<FileManagerWindow> fileManagerWindows)
+        {
+            var total = fileManagerWindows.Count;
+            var pos = 1m / total;
+            for (int i = 0; i < fileManagerWindows.Count; i++)
+            {
+                fileManagerWindows[i].UpdateDisplayDetails(pos, i * pos);
+            }
+        }
 
         public static void Main(string[] args)
         {
@@ -16,46 +31,96 @@ namespace FileManagerCLI
             Console.Title = "FileManager";
             if (File.Exists(ConfigFileName))
             {
-                var json = System.IO.File.ReadAllText(ConfigFileName);
+                var json = File.ReadAllText(ConfigFileName);
                 var config = System.Text.Json.JsonSerializer.Deserialize<Config>(json);
                 Config = config;
             }
 
-            var display1 = new FileManagerWindow();
+            var displays = new List<FileManagerWindow>();
+            var size = new Size(Console.WindowWidth, Console.WindowHeight);
+            displays.Add(new FileManagerWindow(1, 0));
+            var selectedDisplay = displays.First();
             while (true)
             {
+                if (size.Width != Console.WindowWidth || size.Height != Console.WindowHeight)
+                {
+                    size = new Size(Console.WindowWidth, Console.WindowHeight);
+                    foreach (var item in displays)
+                    {
+                        item.Redraw();
+                    }
+                }
+
+
                 var readKey = Console.ReadKey(true);
                 switch (readKey.Key)
                 {
+                    case ConsoleKey.LeftArrow:
+                    {
+                        var wasMod = IfMod(readKey.Modifiers, () =>
+                        {
+                            displays = displays.Take(displays.IndexOf(selectedDisplay) + 1).ToList();
+                            ChangeDisplays(displays);
+                            return true;
+                        });
+
+                        if (!wasMod && selectedDisplay != displays.First())
+                        {
+                            selectedDisplay = displays[displays.IndexOf(selectedDisplay) - 1];
+                        }
+                    }
+                        break;
+                    case ConsoleKey.RightArrow:
+                    {
+                        var wasMod = IfMod(readKey.Modifiers, () =>
+                        {
+                            displays.Add(new FileManagerWindow(0, 0));
+                            ChangeDisplays(displays);
+                            selectedDisplay = displays.Last();
+                            return true;
+                        });
+
+                        if (!wasMod && selectedDisplay != displays.Last())
+                        {
+                            selectedDisplay = displays[displays.IndexOf(selectedDisplay) + 1];
+                        }
+                    }
+                        break;
                     case ConsoleKey.UpArrow:
-                        display1.MoveSelected(true);
+                        selectedDisplay.MoveSelected(true);
                         break;
                     case ConsoleKey.DownArrow:
-                        display1.MoveSelected(false);
+                        selectedDisplay.MoveSelected(false);
                         break;
                     case ConsoleKey.Enter:
-                        display1.Select();
+                        selectedDisplay.Select();
+                        break;
+                    case ConsoleKey.L:
+                        IfMod(readKey.Modifiers, () => selectedDisplay.EditLocation());
                         break;
                     case ConsoleKey.H:
-                        display1.ToggleHidden();
+                        selectedDisplay.ToggleHidden();
+                        break;
+                    case ConsoleKey.Q:
+                        IfMod(readKey.Modifiers, () => Process.GetCurrentProcess().Kill());
                         break;
                     case ConsoleKey.M:
-                        display1.Move();
+                        selectedDisplay.Move();
                         break;
                     case ConsoleKey.C:
-                        display1.Copy();
+                        selectedDisplay.Copy();
                         break;
                     case ConsoleKey.D:
-                        IfMod(readKey.Modifiers, display1.Delete);
+                        IfMod(readKey.Modifiers, selectedDisplay.Delete);
                         break;
                     case ConsoleKey.S:
                         if (!IfMod(readKey.Modifiers, () =>
                             {
-                                display1.ClearStore();
+                                selectedDisplay.ClearStore();
                                 return true;
                             }))
                         {
-                            display1.Store();
+                            selectedDisplay.Store();
                         }
 
                         break;
