d31e05d695e2686e6ad840dc8fbe6c376286601b
mkbmain <michael.bourke@live.co.uk>
2026-03-07 04:51:15 +0000

    wq

--- FileManagerCLI/Data/IoItem.cs
+++ FileManagerCLI/Data/IoItem.cs
@@ -5,22 +5,17 @@ namespace FileManagerCLI.Data;
 
 public class IoItem : IoItemDetails
 {
-    public string DisplayName => 
-        IoType == IoItemType.Directory ? $"{DisplaySize} {BuildDisplay($"\ud83d\udcc1", Program.Config.FolderIconsOptions)}{FileIoUtil.PathSeparator}{Name}" :
-        IoType == IoItemType.File ? $"{DisplaySize} {BuildDisplay($"\ud83d\uddce", Program.Config.FileIconsOptions)}{Name}" :
-        Name;
-
-    private static string BuildDisplay(string icon, IconsOptions iconsOptions)
+    public string DisplayName => IoType switch
     {
-        switch (iconsOptions)
-        {
-            case IconsOptions.Show:
-                return icon;
-            case IconsOptions.ShowWithPadding:
-                return $"{icon} ";
-            case IconsOptions.Hide:
-            default:
-                return string.Empty;
-        }
-    }
+        IoItemType.Directory => $"{DisplaySize} {BuildDisplay($"\ud83d\udcc1", Program.Config.FolderIconsOptions)}{FileIoUtil.PathSeparator}{Name}",
+        IoItemType.File => $"{DisplaySize} {BuildDisplay($"\ud83d\uddce", Program.Config.FileIconsOptions)}{Name}",
+        _ => Name
+    };
+
+    private static string BuildDisplay(string icon, IconsOptions iconsOptions) => iconsOptions switch
+    {
+        IconsOptions.Show => icon,
+        IconsOptions.ShowWithPadding => $"{icon} ",
+        _ => string.Empty
+    };
 }
